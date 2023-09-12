04f9a292534a8b4b81f7ce00a0a92c99e33522e1
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-12 12:15:33 -0300

    creating pages for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: '404', component: ErrorPageComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
